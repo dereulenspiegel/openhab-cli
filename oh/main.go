d52478104cdd94e46f927c79f242cbdf4d44347f
Till Klocke <till.klocke@gmail.com>
2015-10-21 18:31:04 +0200

    Communication is now handled inside the openhab client

--- oh/main.go
+++ oh/main.go
@@ -1,17 +1,14 @@
 package main 
 
 import (
-  "net/http"
   "fmt"
   "os"
   "os/user"
-  "bytes"
-  "io"
-  "io/ioutil"
   "log"
   "path"
 
   "github.com/olebedev/config"
+  "github.com/dereulenspiegel/openhab-cli/openhab"
 )
 
 var OpenHabCfg *config.Config
@@ -20,17 +17,21 @@ var OpenHABHost string
 
 func main() {
   LoadConfigFromHome()
+  client := openhab.NewClient(OpenHABHost)
   args := os.Args
-  if len(args) < 3 {
+  if len(args) < 2 {
     fmt.Printf("Wrong usage\n")
     return
   }
   verb := args[1]
-  item := args[2]
 
   switch verb {
   case "state":
-    state, err := GetState(OpenHABHost,item)
+    if len(args) < 3 {
+      log.Fatalf("Print usage")
+    }
+    item := args[2]
+    state, err := client.GetState(item)
     if err != nil {
       HandleError(err)
     } else {
@@ -41,10 +42,19 @@ func main() {
       // TODO print usage
       log.Fatalf("Not enough arguments")
     }
+    item := args[2]
     command := args[3]
-    err := SendCommand(OpenHABHost, item, command)
+    err := client.SendCommand(item, command)
     if err != nil {
       HandleError(err)
+    }
+  case "list":
+    items, err := client.ListItems()
+    if err != nil {
+      HandleError(err)
+    }
+    for _, item := range items {
+      fmt.Printf("%s\t\t%s\n", item.Name, item.State)
     }
   default:
     log.Fatalf("Unknown command %s",verb)
@@ -70,64 +80,3 @@ func LoadConfigFromHome() {
   }
 }
 
-func NewOpenHabRequest(method, url, contentType, payload string) (*http.Request, error) {
-  var payloadReader io.Reader = nil
-  if payload != "" {
-    payloadReader = bytes.NewReader([]byte(payload))
-  }
-  req, err := http.NewRequest(method, url, payloadReader)
-  if err != nil {
-    return nil,err
-  }
-  if contentType != "" {
-    req.Header.Add("Content-Type",contentType)
-  }
-  if payload != "" {
-    req.Header.Add("Accept", "application/json")
-  }
-  return req, nil
-}
-
-func ReaderToString(reader io.ReadCloser) string {
-  defer reader.Close()
-  body, _ := ioutil.ReadAll(reader)
-  return string(body)
-}
-
-func GetState(host, item string) (string,error) {
-  client := &http.Client{}
-  url := fmt.Sprintf("%s/rest/items/%s/state",host, item)
-  req, err := NewOpenHabRequest("GET", url, "","")
-  if err != nil {
-    return "",err
-  }
-  resp, err := client.Do(req)
-  if err != nil {
-    return "", nil
-  }
-
-  if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-    body := ReaderToString(resp.Body)
-    return body, nil
-  } else {
-    errorBody := ReaderToString(resp.Body)
-    return "", fmt.Errorf("Error response %d %s",resp.StatusCode, errorBody)
-  }
-
-}
-
-func SendCommand(host, item, command string) error {
-  client := &http.Client{}
-  req, err := NewOpenHabRequest("POST", fmt.Sprintf("%s/rest/items/%s", host, item), "text/plain",command)
-  if err != nil {
-    return err
-  }
-  resp, err := client.Do(req)
-  if err != nil {
-    return err
-  }
-  if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-    log.Fatalf("Received error from server %d %s", resp.StatusCode, ReaderToString(resp.Body))
-  }
-  return nil
-}
---
+++ openhab/openhab.go
@@ -0,0 +1,108 @@
+package openhab
+
+import (
+  "net/http"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "bytes"
+  "encoding/json"
+)
+
+type Client struct {
+  httpClient *http.Client
+  host       string
+}
+
+type Item struct {
+  Type string `json:"type"`
+  Name string `json:"name"`
+  State string `json:"state"`
+  Link string `json:"link"`
+}
+
+type ItemsResponse struct {
+  Items []Item `json:"item"`
+}
+
+func NewClient(host string) *Client {
+  return &Client{
+    httpClient: &http.Client{},
+    host: host,
+  }
+}
+
+func (c *Client) GetState(item string) (string,error) {
+  url := fmt.Sprintf("%s/rest/items/%s/state", c.host, item)
+  req, err := newRequest("GET",url,"","")
+  if err != nil {
+    return "", nil 
+  }
+  return c.executeRequest(req)
+}
+
+func (c *Client) SendCommand(item, command string) error {
+  url := fmt.Sprintf("%s/rest/items/%s", c.host, item)
+  req, err := newRequest("POST",url,"text/plain",command)
+  if err != nil {
+    return err
+  }
+  _, err = c.executeRequest(req)
+  return err
+}
+
+func (c *Client) ListItems() ([]Item, error) {
+  url := fmt.Sprintf("%s/rest/items", c.host)
+  req, err := newRequest("GET",url,"","")
+  if err != nil {
+    return nil, err
+  }
+  req.Header.Add("Accept", "application/json")
+  body, err := c.executeRequest(req)
+  if err != nil {
+    return nil, err
+  }
+  itemsResponse := &ItemsResponse{}
+  err = json.Unmarshal([]byte(body), itemsResponse)
+  if err != nil {
+    return nil, err
+  }
+  return itemsResponse.Items,nil
+}
+
+func (c *Client) executeRequest(req *http.Request) (string, error) {
+  resp, err := c.httpClient.Do(req)
+  if err != nil {
+    return "", nil
+  }
+  body := readerToString(resp.Body)
+  if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+    return body, nil
+  } else {
+    return "", fmt.Errorf("Got error from server %d",resp.StatusCode)
+  }
+}
+
+func readerToString(reader io.ReadCloser) string {
+  defer reader.Close()
+  body, _ := ioutil.ReadAll(reader)
+  return string(body)
+}
+
+func newRequest(method, url, contentType, payload string) (*http.Request, error) {
+  var payloadReader io.Reader = nil
+  if payload != "" {
+    payloadReader = bytes.NewReader([]byte(payload))
+  }
+  req, err := http.NewRequest(method, url, payloadReader)
+  if err != nil {
+    return nil,err
+  }
+  if contentType != "" {
+    req.Header.Add("Content-Type",contentType)
+  }
+  if payload != "" {
+    req.Header.Add("Accept", "application/json")
+  }
+  return req, nil
+}
