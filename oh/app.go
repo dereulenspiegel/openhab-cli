7735c7c82e64269e3bf4f062da58d28e42e2632f
Till Klocke <till.klocke@gmail.com>
2015-10-28 19:24:03 +0100

    The configuration is now generated if not found

--- oh/app.go
+++ oh/app.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/user"
 	"path"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/dereulenspiegel/openhab-cli/oh/commands"
@@ -34,6 +39,34 @@ func LoadConfigFromHome() {
 	}
 }
 
+func AskForHostAndRenderConfig() {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal("Can't obtain current user")
+	}
+	configPath := path.Join(usr.HomeDir, ".openhab-cli")
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Please enter openHAB host URL:\n")
+		host, _ := reader.ReadString('\n')
+		cfg := map[string]interface{}{
+			"host": host,
+		}
+		cfgContent, err := config.RenderYaml(cfg)
+		if err != nil {
+			log.Fatal("Can't render config")
+		}
+		cfgContent = strings.Replace(cfgContent, "|", "", -1)
+		cfgContent = strings.Replace(cfgContent, "\n", "", -1)
+		cfgContent = strings.TrimSpace(cfgContent)
+		err = ioutil.WriteFile(configPath, []byte(cfgContent), 0644)
+		if err != nil {
+			log.Fatal("Can't write config")
+		}
+	}
+}
+
 func CollectCommands() []cli.Command {
 	commandList := make([]cli.Command, 0, 10)
 	commandList = append(commandList, commands.NewListCommand(OpenHABClient))
@@ -44,6 +77,7 @@ func CollectCommands() []cli.Command {
 }
 
 func CreateCliApp() *cli.App {
+	AskForHostAndRenderConfig()
 	LoadConfigFromHome()
 	OpenHABClient = openhab.NewClient(OpenHABHost)
 	App = cli.NewApp()
